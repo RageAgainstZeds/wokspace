45ae2db47705a2738ee1f86e5f9596fea3613151
RageAgainst <uuvulaulu@gmail.com>
2017-10-22 21:37:43 +0300

    making pankes

--- homework/Triangle/Main.java
+++ homework/Triangle/Main.java
@@ -3,6 +3,12 @@ package com.homework.Triangle;
 public class Main {
 	public static void main(String[] args) {
 		//code
-		System.out.println("Hello!");
+		Triangle triangle1 = new Triangle(-3, 6, -6);
+		Triangle triangle2 = new Triangle(10, 10, 10);
+		Triangle triangle3 = new Triangle(13, 20, 8);
+		
+		System.out.println(triangle1.area());
+		System.out.println(triangle2.area());
+		System.out.println(triangle3.area());
 	}
 }
--- homework/Triangle/Triangle.java
+++ homework/Triangle/Triangle.java
@@ -1,5 +1,52 @@
 package com.homework.Triangle;
 
+import java.lang.Math;
+
+
 public class Triangle {
+	private double a;
+	private double b;
+	private double c;
+	
+	
+	
+	public Triangle(double a, double b, double c) {
+		super();
+		
+		this.a = Math.abs(a);
+		this.b = Math.abs(b);
+		this.c = Math.abs(c);
+	}
+	
+	public Triangle() {
+		
+	}
+	
+	public double getA() {
+		return a;
+	}
+	public void setA(double a) {
+		this.a = a;
+	}
+	public double getB() {
+		return b;
+	}
+	public void setB(double b) {
+		this.b = b;
+	}
+	public double getC() {
+		return c;
+	}
+	public void setC(double c) {
+		this.c = c;
+	}
+	
+	
+	
+	public double area() {
+		double p = (a + b+ c)/2;
+		double S = Math.sqrt(p*(p-a)*(p-b)*(p-c));
+		return S;
+	}
 	
 }
