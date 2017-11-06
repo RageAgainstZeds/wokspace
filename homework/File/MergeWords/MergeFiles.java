be3aa56aa93f3b1d83972cc523c75e28a7b0f3f5
RageAgainst <uuvulaulu@gmail.com>
2017-11-06 21:38:21 +0200

    making pancakes

---
+++ homework/File/MergeWords/MergeFiles.java
@@ -0,0 +1,100 @@
+package com.homework.File.MergeWords;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MergeFiles {
+	
+	private File fileOne;
+	private File fileTwo;
+	private List<String> matchList = new ArrayList<>();
+	private File fileMatches;
+	private List<String> txtOne = new ArrayList<>();
+	private List<String> txtTwo = new ArrayList<>();
+	private File[] fileList;
+	
+	public MergeFiles(String fileOne, String fileTwo, String fileMatches) {
+		super();
+		this.fileOne = new File(fileOne);
+		this.fileTwo = new File(fileTwo);
+		this.fileMatches = new File(fileMatches);
+		this.fileList[0] = this.fileOne;
+		this.fileList[1] = this.fileTwo;
+	}
+	public MergeFiles() {
+		super();
+	}
+		
+	public File getFileOne() {
+		return fileOne;
+	}
+	public void setFileOne(File fileOne) {
+		this.fileOne = fileOne;
+	}
+	public File getFileTwo() {
+		return fileTwo;
+	}
+	public void setFileTwo(File fileTwo) {
+		this.fileTwo = fileTwo;
+	}
+	public File getFileMatches() {
+		return fileMatches;
+	}
+	public void setFileMatches(File fileMatches) {
+		this.fileMatches = fileMatches;
+	}
+	
+	
+	public void readFiles() {
+		for (File file : fileList) {
+			try (BufferedReader fileIn = new BufferedReader(new FileReader(file))) {
+	            //reading lines from a file and parse it
+	            String str;
+	            while ((str = fileIn.readLine()) != null) {
+	            	for (String retval : str.split(" ")) {
+	            		if (file.getName().equals(fileList[0].getName())) {
+	            			txtOne.add(retval);
+	            		} else if (file.getName().equals(fileList[1].getName())) {
+	            			txtTwo.add(retval);
+	            		}
+    	            }
+            	}
+            } catch (IOException ex) {
+	            ex.getMessage();
+	        }
+		}
+	}
+	
+	public void writeWords() {
+		//get the repeated words from two lists
+				for (int i = 0; i < txtOne.size(); i++) {
+					if (txtTwo.contains(txtOne.get(i))) {
+						matchList.add(txtOne.get(i));
+					}
+				}
+				//get only distinct words
+				matchList = matchList.stream().distinct().collect(Collectors.toList());
+				//File Writer
+		        try(FileWriter fileOut = new FileWriter(fileMatches)) {
+		            //write final list into the file
+		            Iterator<String> iter = matchList.iterator();
+		            while (iter.hasNext()) {
+		                String txt = (String) iter.next();
+		                fileOut.write(txt + " ");
+		                System.out.println("File Write Successfully!");
+		            }
+		            fileOut.flush();
+		            fileOut.close();
+		        }
+		        catch (IOException ex) {
+		            System.out.println(ex.getMessage());
+		        }
+	}
+}
